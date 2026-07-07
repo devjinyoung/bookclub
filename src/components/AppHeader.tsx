1b1d25ac1c022aa164f52f6efd651bb6ddb59605
youngbbcoconut <70621556+youngbbcoconut@users.noreply.github.com>
2026-07-07 20:08:39 +0100

    conditional render

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -93,6 +93,12 @@ export function AppHeader() {
     }
   }
 
+  if (!profile || isLoading) {
+    return (
+      <header className="sticky top-0 z-10 flex w-full max-w-md min-h-12 items-center justify-between gap-2 border-b border-slate-800 bg-slate-950/95 px-2 backdrop-blur supports-[backdrop-filter]:bg-slate-950/80"></header>
+    );
+  }
+
   return (
     <header className="sticky top-0 z-10 flex w-full max-w-md items-center justify-between gap-2 border-b border-slate-800 bg-slate-950/95 px-2 backdrop-blur supports-[backdrop-filter]:bg-slate-950/80">
       <button
